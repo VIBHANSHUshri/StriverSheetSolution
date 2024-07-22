feabae7317fb6341f3a7a3906a5f4de9dcee2089
VIBHANSHUshri <vibhanshu123khare@gmail.com>
2024-07-22 08:03:39 +0530

    new

--- Binarysearch/Capacity.cpp
+++ Binarysearch/Capacity.cpp
@@ -44,3 +44,47 @@ public:
 
 
 //Binary search approach
+class Solution {
+public:
+    int shipWithinDays(vector<int>& arr, int days) {
+         //BASIC APPROACH
+         int sum = 0;
+         int maxi = INT_MIN;
+         for(int i = 0 ; i < arr.size() ; i++)
+         {
+            sum = sum + arr[i];
+            maxi = max(arr[i] , maxi);
+         }
+         int low = maxi;
+         int high = sum;
+
+         while(low <= high)
+         {
+             int mid = (low + high) / 2;
+               int total = 0;
+               int count = 1;
+        
+            for(int j = 0 ; j < arr.size() ; j++)
+            {
+                 if(total + arr[j] > mid)
+                 {
+                    //capactiy full
+                    count++;
+                    total = arr[j];
+                 } 
+                 else{
+                       total = total + arr[j];
+                 }
+            }
+            if (count <= days) {
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+          
+         }
+       return low;
+                   
+    }
+};
